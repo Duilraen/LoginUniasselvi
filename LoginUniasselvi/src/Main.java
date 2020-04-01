cbc620655cc8d4e340e1ff4fd0b9fd96aa0674f8
guigoinfosis <guigoinfosis@gmail.com>
2020-04-01 03:16:17 -0300

    Inclusao da classes bases do app LoginUniasselvi

---
+++ LoginUniasselvi/src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+
+	public static void main(String[] args) {
+	PessoaFisica Pessoa1Pessoa = new PessoaFisica();	
+    Pessoa1Pessoa.setNome("Guilherme");
+    Pessoa1Pessoa.setCpf("0987654321");
+    //**/
+    System.out.println(Pessoa1Pessoa.getNome());
+	}
+
+}
