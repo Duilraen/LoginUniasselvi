a86bdf59560461bc6bd16cc69afae1c2055f98d5
guigoinfosis <guigoinfosis@gmail.com>
2020-04-25 20:05:26 -0300

    Atualizacao da lista de comandos

---
+++ LoginUniasselvi/src/Funcionario.java
@@ -0,0 +1,44 @@
+
+public class Funcionario {
+	private String nome;
+	private String cpf;
+	private double salario;
+	
+	public Funcionario() {
+		this.nome = "";
+		this.cpf = "";
+		this.salario = 0.0;
+	}
+	
+	public String getNome() {
+		return nome;
+	}
+	
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+	public String getCpf() {
+		return cpf;
+	}
+	
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+	
+	public double getSalario() {
+		return salario;
+	}
+	
+	public void setSalario(double salario) {
+		this.salario = salario;
+	}
+	
+	public double getBonificacao() {
+		return this.salario * 0.10;
+	}
+	
+	public String toString() {
+		return "Nome: " + this.getNome() + " / CPF: " + this.getCpf() + " / Salario: " + this.getSalario();
+	}
+}
